0c259a79b7d91261bb9f5462536ed9ff4e2693c6
Axdr <93472320+grvelvet@users.noreply.github.com>
2025-09-12 22:38:38 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,5 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 
-// БЫЛО:
-// export default defineConfig({
- base: '/prompt-artisan/',
-// })
-
-// СТАЛО:
 export default defineConfig({
-  // base теперь нет
+  base: '/prompt-artisan/',
 })
