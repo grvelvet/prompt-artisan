ae82c7e24548505d7d7c62ab173083c5ad2cae04
Axdr <93472320+grvelvet@users.noreply.github.com>
2025-09-12 23:20:30 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,16 @@
 import { defineConfig } from 'vite'
-export default defineConfig({
-base: '/prompt-artisan/',
+
+// Эта функция будет автоматически определять, какая команда запущена
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    // Если это локальная разработка (npm run dev)
+    return {
+      // Никакого `base` не нужно
+    }
+  } else {
+    // Если это сборка для публикации (npm run build)
+    return {
+      base: '/prompt-artisan/',
+    }
+  }
 })
